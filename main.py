6dd8125922be55f36dcbd389ad235fa26bb1e7ca
Ken Qu <43532683+adasken@users.noreply.github.com>
2023-05-23 09:42:20 +1000

    Update main.py

--- main.py
+++ main.py
@@ -13,8 +13,8 @@ import base64
 app = Flask(__name__)
 
 ### TO EDIT ###
-sql_host = '35.244.69.6' 
-sql_connection_name='cloud-computing-387303:australia-southeast1:googlecloudsql'
+sql_host = '34.87.227.182' 
+sql_connection_name='cloud-computing-387522:australia-southeast1:googlecloudsql'
 sql_port = 3306
 sql_database = 'emotions'
 sql_user = 'root'
